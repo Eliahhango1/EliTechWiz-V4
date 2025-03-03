751f6bee673b40be229146712f5786ef147b6ec7
Eliahhango <146315119+Eliahhango@users.noreply.github.com>
2025-03-03 22:35:15 +0300

    Update bongo.js

--- commandes/bongo.js
+++ commandes/bongo.js
@@ -42,7 +42,7 @@ zokou({
   } = _0x20339c;
   const _0x5754a8 = await fetch("https://nekos.life/api/v2/fact");
   const _0x21e127 = await _0x5754a8.json();
-  _0x12e23a(" *BMW MD FACT MESSAGE* \n*💠* " + _0x21e127.fact + "\n\n\n\n\n*©Eliah Hango*\n\n╔═════◇\n╏◇ *EliTechWiz-V4*\n╚════════════════════>  ");
+  _0x12e23a(" *EliTechWiz-V4  FACT MESSAGE* \n*💠* " + _0x21e127.fact + "\n\n\n\n\n*©Eliah Hango*\n\n╔═════◇\n╏◇ *EliTechWiz-V4*\n╚════════════════════>  ");
 });
 zokou({
   'nomCom': "quote",
