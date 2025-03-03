622cef1ccbc5d500e0575bec1deb6529d8c62f3b
Eliahhango <146315119+Eliahhango@users.noreply.github.com>
2025-03-03 23:52:39 +0300

    Update groupe.js

--- commandes/groupe.js
+++ commandes/groupe.js
@@ -31,7 +31,7 @@ zokou({ nomCom: "tagall", categorie: 'Group', reaction: "📣" }, async (dest, z
   let membresGroupe = verifGroupe ? await infosGroupe.participants : ""
   var tag = ""; 
   tag += `========================\n  
-        🌟 *Eliah-V𝐌𝐃*𝕋𝔸𝔾* 🌟
+        🌟 *EliTechWiz-V4 TAG* 🌟
 ========================\n
 👥 Group : ${nomGroupe} 🚀 
 👤 Autor : *${nomAuteurMessage}* 👋 
